af7e3f57ef32c955d7383113a5f16fa8434ce8ea
Fahad <fahad.shoukat111@gmail.com>
2024-09-23 16:57:40 +0500

    figma to react web app minor-change

--- src/sections/demo.jsx
+++ src/sections/demo.jsx
@@ -9,7 +9,29 @@ const Demo = () => {
         Pellentesque suscipit fringilla <br /> libero eu.{" "}
       </h1>
       <div className="mb-4">
-        <Button text="Get A Demo" />
+
+        <button
+          className="inline-flex items-center gap-2 rounded border border-[#4CAF4F] bg-[#4CAF4F] px-8 py-3 text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
+          href="#"
+        >
+          <span className="text-sm font-medium"> Get a Demo </span>
+
+          <svg
+            className="size-5 rtl:rotate-180"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M17 8l4 4m0 0l-4 4m4-4H3"
+            />
+          </svg>
+        </button>
+
       </div>
     </div>
   );
